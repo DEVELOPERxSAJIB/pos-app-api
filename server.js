07b4ac24228d2bb1bf90a3e955f28afed89609bf
DEVELOPERxSAJIB <sajibshikder78971@gmail.com>
2024-02-16 17:27:07 +0600

    update cors settings

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://pos-app-client-zarj.onrender.com",
+    ],
     credentials: true,
   })
 );
